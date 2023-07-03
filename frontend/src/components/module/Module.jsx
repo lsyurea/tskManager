5c5874f6974d97627c2dfa75e647d4081cf1f2d2
leesiyuan <lee.si.yuan.20@gmail.com>
2023-07-03 17:17:41 +0800

    Allow access to module only after logging in

--- frontend/src/components/module/Module.jsx
+++ frontend/src/components/module/Module.jsx
@@ -1,6 +1,17 @@
 import './Module.css'
 
 function Module() {
+    const user = () => {
+        const token = JSON.parse(sessionStorage.getItem('token'));
+        return token ? token.user : token;
+    }
+    if (!user()) return (
+    <>
+    <div>
+        <h1>Please login first</h1>
+    </div>
+    </>);
+    
     return (
         <div className='module'>
             <h1>Modules</h1>
