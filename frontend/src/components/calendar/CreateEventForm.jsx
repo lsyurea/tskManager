27c2df25441c29ed50d9a31972252de1306a5d37
Nah Choon Yan <choonyan02@gmail.com>
2023-07-04 19:04:09 +0800

    Updated UI/UX of EventForm
    As per title

--- frontend/src/components/calendar/CreateEventForm.jsx
+++ frontend/src/components/calendar/CreateEventForm.jsx
@@ -1,9 +1,9 @@
-
+import './CreateEventForm.css'
+import { useEffect } from 'react';
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { supabase } from '../../helper/SupabaseClient'
-import './CreateEventForm.css'
 
 function CreateEventForm( {extraFunction}) {
   const [title, setTitle] = useState("");
@@ -14,12 +14,6 @@ function CreateEventForm( {extraFunction}) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Validate form inputs
-    if (!title || !startDate || !endDate) {
-      alert("Please fill in all fields");
-      return;
-    }
 
     // Create new event object
     addEvent();
@@ -46,37 +40,39 @@ function CreateEventForm( {extraFunction}) {
     }
   };
 
+  useEffect(() => {
+    const script1 = document.createElement('script');
+    script1.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js';
+    script1.type = 'module';
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js';
+    script2.setAttribute('nomodule', '');
+    document.body.appendChild(script2);
+
+    return () => {
+      // Cleanup script tags if necessary
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
   return (
     <div className="eventCard">
+      <a href='/calendar'><span className="icon-close"><ion-icon name="close"></ion-icon></span></a>
       <form className="eventForm" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+        <div className="input-box">
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} required/>
+          <label htmlFor="title">Title</label>
         </div>
-        <div>
-          <label htmlFor="startDate">Start Date:</label>
-          <DatePicker
-            id="startDate"
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            showTimeSelect
-            dateFormat="Pp"
-          />
+        <div className="date-picker">
+          <DatePicker id="startDate" selected={startDate} onChange={(date) => setStartDate(date)} showTimeSelect dateFormat="Pp" required/>
+          <label htmlFor="startDate">Start Date</label>
         </div>
-        <div>
-          <label htmlFor="endDate">End Date:</label>
-          <DatePicker
-            id="endDate"
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            showTimeSelect
-            dateFormat="Pp"
-          />
+        <div className="date-picker">
+          <DatePicker id="endDate" selected={endDate} onChange={(date) => setEndDate(date)} showTimeSelect dateFormat="Pp" required/>
+          <label htmlFor="endDate">End Date</label>
         </div>
         <button className="btn" type="submit">Create Event</button>
       </form>
