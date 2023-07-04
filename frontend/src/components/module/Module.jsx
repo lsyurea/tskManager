c4d9ae1e1a21a5974950cb18f595d05447556a98
leesiyuan <lee.si.yuan.20@gmail.com>
2023-07-04 11:04:39 +0800

    integrate nus mods API

--- frontend/src/components/module/Module.jsx
+++ frontend/src/components/module/Module.jsx
@@ -1,5 +1,22 @@
 import './Module.css'
 
+const apiUrl = 'https://api.nusmods.com/v2/modules.json';
+
+// Function to fetch all modules
+async function fetchAllModules() {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    const modules = await response.json();
+    return modules;
+  } catch (error) {
+    console.error('Error fetching modules:', error);
+    return null;
+  }
+}
+
 function Module() {
     const user = () => {
         const token = JSON.parse(sessionStorage.getItem('token'));
@@ -11,7 +28,9 @@ function Module() {
         <h1>Please login first</h1>
     </div>
     </>);
-    
+
+    console.log(fetchAllModules())
+
     return (
         <div className='module'>
             <h1>Modules</h1>
