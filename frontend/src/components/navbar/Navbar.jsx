8ead36c1085333aa1a5b19463597452a5f8648c5
leesiyuan <lee.si.yuan.20@gmail.com>
2023-07-09 04:06:14 +0800

    added navbar responsiveness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,6 +1,14 @@
+
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { useRef } from 'react'
 import './Navbar.css'
 
 function Navbar() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
 
     const loginPopup = () => {
         window.location.href = '/login'
@@ -11,18 +19,22 @@ function Navbar() {
         window.location.href = '/login'
     }
 
-
     return (
         <div className='header'>
             <h2 className="logo">tskManager</h2>
-            <nav className="navigation">
+            <nav ref={navRef} className="navigation">
                 <a href="/">Module Details</a>
                 <a href="/calendar">Calendar</a>
                 <a href="/dashboard">Todo</a>
                 <a href="/module">Profile</a>
                 {sessionStorage.getItem('token') ? <button className="btnLogin-popup" onClick={logout}>Logout</button> : <button className="btnLogin-popup" onClick={loginPopup}>Login</button>}
-                
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
             </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
         </div>
     )
 }
